2b9e9c80e70cb7c8b431554db995304d64930317
Mongudd <eugen991215@gmail.com>
2024-12-13 13:10:43 +0900

    Update app.js - Arrays

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
 
+// Get Item from Array
+console.log(daysOfWeek[0]);
+console.log(daysOfWeek[4]);
+console.log(daysOfWeek);
+
+// Add one more day to the array
+daysOfWeek.push("sun");
+console.log(daysOfWeek);
