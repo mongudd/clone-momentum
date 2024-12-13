ea250a65e15146f66a7d14b12cd2bd8ddfb7154d
Mongudd <eugen991215@gmail.com>
2024-12-13 15:03:23 +0900

    Update app.js - Functions

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
+function sayHello(nameOfPerson, age) {
+  console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
+}
+sayHello("mongudd", 10);
+sayHello("nico", 23);
+sayHello("dal", 21);
+
+//
+function plus(firstNumber, secondNumber) {
+  console.log(firstNumber + secondNumber);
+}
+plus(60, 8);
+
+function divide(a, b) {
+  console.log(a / b);
+}
+divide(98, 20);
+
+//
 const player = {
   name: "mongudd",
-  points: 10,
-  fat: true,
+  sayHello: function (otherPersonsName) {
+    console.log("hello! " + otherPersonsName + " nice to meet you!");
+  },
 };
-
 console.log(player.name);
-console.log(player["name"]);
-console.log(player);
-// Update objects
-player.fat = false;
-console.log(player);
+player.sayHello("lynn");
 
-player.lastName = "potato";
-player.points = player.points + 20;
-console.log(player);
+// Simple
+function plus(a, b) {
+  console.log(a + b);
+}
+plus(5, 6);
