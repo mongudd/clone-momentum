9aa269514bd27af0c4d9347f4fe55a198bbca6d7
Mongudd <eugen991215@gmail.com>
2024-12-14 14:05:56 +0900

    Update app.js - Conditional

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
-const calculator = {
-  plus: function (a, b) {
-    return a + b;
-  },
-  minus: function (a, b) {
-    return a - b;
-  },
-  multiple: function (a, b) {
-    return a * b;
-  },
-  divide: function (a, b) {
-    return a / b;
-  },
-  squareRoot: function (a, b) {
-    return a ** b;
-  },
-};
+//parseInt === string -> number
+const age = parseInt(prompt("How old are you?"));
 
-const plusResult = calculator.plus(2, 3);
-const minusResult = calculator.minus(plusResult, 10);
-const multipleResult = calculator.multiple(10, minusResult);
-const divideResult = calculator.divide(multipleResult, plusResult);
-const squareRootResult = calculator.squareRoot(divideResult, minusResult);
+console.log(age);
+console.log(isNaN(age));
+
+// condition === boolean
+
+// && === And
+// true && true === true
+// true && false === false
+// false && true === false
+// false && false === false
+
+// || === Or
+// true || true === true
+// false || ture === true
+// true || false === true
+// false || false === false
+
+if (isNaN(age) || age < 0) {
+  console.log("Please write a real positive number.");
+} else if (age < 18) {
+  console.log("You are too young.");
+} else if (age >= 18 && age <= 50) {
+  console.log("You can drink.");
+} else if (age > 50 && age <= 80) {
+  console.log("You better not to drink.");
+} else if (age === 100) {
+  console.log("Wow, you are wise.");
+} else if (age > 80) {
+  console.log("You can do whatever you want.");
+}
