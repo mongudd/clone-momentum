0bf785df880603a960ed620d25e2c4e76061f7fe
Mongudd <eugen991215@gmail.com>
2024-12-14 16:19:20 +0900

    CSS~JS

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
-function handleh1Click() {
-  const currentColor = h1.style.color;
-  let newColor;
-  if (currentColor === "blue") {
-    newColor = "tomato";
+// existing class name is changing by JS
+// sexy-font -> clicked
+/*function handleh1Click() {
+  const clickedClass = "clicked";
+  if (h1.className === clickedClass) {
+    h1.className = "";
   } else {
-    newColor = "blue";
+    h1.className = clickedClass;
   }
-  h1.style.color = newColor;
+} */
+
+// To maintain existing class name === className -> classList
+// sexy-font + clicked
+/*function handleh1Click() {
+  const clickedClass = "clicked";
+  if (h1.classList.contains(clickedClass)) {
+    h1.classList.remove(clickedClass);
+  } else {
+    h1.classList.add(clickedClass);
+  }
+} */
+
+// To shorten === classList.contains.remove.add -> toggle
+// sexy-font + clicked
+function handleh1Click() {
+  h1.classList.toggle("clicked");
 }
 
 h1.addEventListener("click", handleh1Click);
