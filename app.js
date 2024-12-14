cc8124fed56fc7aba5ac58f2126126aece8df2ce
Mongudd <eugen991215@gmail.com>
2024-12-14 15:54:50 +0900

    HTML~JS - Styles (CSS function)

--- app.js
+++ app.js
@@ -1,45 +1,14 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
-console.dir(h1);
-
 function handleh1Click() {
-  h1.style.color = "blue";
+  const currentColor = h1.style.color;
+  let newColor;
+  if (currentColor === "blue") {
+    newColor = "tomato";
+  } else {
+    newColor = "blue";
+  }
+  h1.style.color = newColor;
 }
 
-function handleMouseEnter() {
-  h1.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave() {
-  h1.innerText = "Mouse is gone!";
-}
-
-//If use in this way, can removeEventLister later
 h1.addEventListener("click", handleh1Click);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-//h1.onclick = handleh1Click;
-//h1.onmouseenter = handleMouseEnter;
-//h1.onmouseleave = handleMouseLeave;
-
-function handelWindowResize() {
-  document.body.style.backgroundColor = "tomato";
-}
-
-function handelWindowCopy() {
-  alert("Copier!");
-}
-
-function handelWindowOffline() {
-  alert("SOS no Wifi");
-}
-
-function handelWindowOnline() {
-  alert("ALL GOOD!");
-}
-
-window.addEventListener("resize", handelWindowResize);
-window.addEventListener("copy", handelWindowCopy);
-window.addEventListener("offline", handelWindowOffline);
-window.addEventListener("online", handelWindowOnline);
