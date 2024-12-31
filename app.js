e4990fe1b47c6b82adbde0bef2fad6efbdd6a55d
Mongudd <eugen991215@gmail.com>
2024-12-31 22:34:03 +0900

    Login - Saving Username

--- app.js
+++ app.js
@@ -6,15 +6,9 @@ const loginInput = loginForm.querySelector("input");
 const loginInput = document.querySelector("#login-form input");
 const loginBtn = document.querySelector("#login-form button"); */
 
-// 유저한테 인사하기!
-/* form 제출 + 기본동작 막기 -> value 저장 -> form 숨기기
-function onLoginSubmit(event) {
-  event.preventDefault();
-  const username = loginInput.value;
-  loginForm.classList.add("hidden");
-  console.log(username);
-} */
-/* form 제출 + 기본 동작 막기 -> form 숨기기 -> value 저장 -> value h1에 입력 -> h1 보이게하기 */
+// 유저 기억하기!
+// form 제출 + 기본 동작 막기 -> form 숨기기 -> value 저장 -> value h1에 입력 -> h1 보이게하기
+// value를 localstorage에 저장하여 기억
 const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden"; // string만 포함된 변수는 대문자로
 
@@ -22,7 +16,7 @@ function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  //greeting.innerText = "Hello " + username;
+  localStorage.setItem("Username", username);
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
