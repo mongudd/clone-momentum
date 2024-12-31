4e4a6f14182ac6577559b49323068eb2eacfcbcb
Mongudd <eugen991215@gmail.com>
2024-12-31 22:33:39 +0900

    Login - Getting Username

--- app.js
+++ app.js
@@ -6,21 +6,25 @@ const loginInput = loginForm.querySelector("input");
 const loginInput = document.querySelector("#login-form input");
 const loginBtn = document.querySelector("#login-form button"); */
 
-// link의 기본 동작 : 클릭시 다른 페이지로 이동 //
-// 해당 기본 동작 막아보기! //
-const link = loginForm.querySelector("a");
+// 유저한테 인사하기!
+/* form 제출 + 기본동작 막기 -> value 저장 -> form 숨기기
+function onLoginSubmit(event) {
+  event.preventDefault();
+  const username = loginInput.value;
+  loginForm.classList.add("hidden");
+  console.log(username);
+} */
+/* form 제출 + 기본 동작 막기 -> form 숨기기 -> value 저장 -> value h1에 입력 -> h1 보이게하기 */
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden"; // string만 포함된 변수는 대문자로
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  console.log(loginInput.value);
-}
-
-function handleLinkClick(event) {
-  event.preventDefault();
-  console.dir(event);
-  // alert("Click!");
-  // alert = 기본 동작을 막을 수 있음. 단, 확인 버튼을 누르기 전 까지만 유효.
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  //greeting.innerText = "Hello " + username;
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
