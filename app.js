5ecd4df60fbb1b9d2ede7fc1d4063974a4a2121c
Mongudd <eugen991215@gmail.com>
2024-12-31 22:27:51 +0900

    Create Calculator

--- app.js
+++ app.js
@@ -1,33 +1,23 @@
-function sayHello(nameOfPerson, age) {
-  console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
-}
-sayHello("mongudd", 10);
-sayHello("nico", 23);
-sayHello("dal", 21);
-
-//
-function plus(firstNumber, secondNumber) {
-  console.log(firstNumber + secondNumber);
-}
-plus(60, 8);
-
-function divide(a, b) {
-  console.log(a / b);
-}
-divide(98, 20);
-
-//
-const player = {
-  name: "mongudd",
-  sayHello: function (otherPersonsName) {
-    console.log("hello! " + otherPersonsName + " nice to meet you!");
+const calculator = {
+  plus: function (a, b) {
+    console.log(a + b);
+  },
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+  multiple: function (a, b) {
+    console.log(a * b);
+  },
+  divide: function (a, b) {
+    console.log(a / b);
+  },
+  squareRoot: function (a, b) {
+    console.log(a ** b);
   },
 };
-console.log(player.name);
-player.sayHello("lynn");
 
-// Simple
-function plus(a, b) {
-  console.log(a + b);
-}
-plus(5, 6);
+calculator.plus(2, 5);
+calculator.minus(2, 5);
+calculator.multiple(2, 5);
+calculator.divide(2, 5);
+calculator.squareRoot(2, 5);
