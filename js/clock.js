509e698fdf1fdb4559d656d7737a541b73536449
Mongudd <eugen991215@gmail.com>
2024-12-31 22:39:19 +0900

    Clock - padStart

--- js/clock.js
+++ js/clock.js
@@ -1,8 +1,14 @@
 const clock = document.querySelector("#clock");
 
+// 시간 표시 방식 00:00:00로 바꾸기!
+// padStart(최대길이, 빈공간을 무엇으로 채울지 "1", "x",,,) : string 앞 padding 추가
+
 function getClock() {
   const date = new Date();
-  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  const hours = String(date.getHours()).padStart(2, "0"); // number -> string : String()으로 감싸기
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 getClock(); // 1초 대기 없이 바로
