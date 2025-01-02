df231ee29d413302f87d35bef9b45501a3ffb25d
Mongudd <eugen991215@gmail.com>
2025-01-02 21:52:12 +0900

    To do - Saving ToDos

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,13 @@ const toDoForm = document.getElementById("todo-form");
 const toDoList = document.getElementById("todo-list");
 const toDoInput = toDoForm.querySelector("input");
 
+const toDos = [];
+
+function saveTodo() {
+  localStorage.setItem("todos", JSON.stringify(toDos)); // object , arrary, whatever JS code -> String
+  // by this code, duplicated value also can be saved
+}
+
 function deleteTodo(event) {
   const li = event.target.parentElement;
   li.remove();
@@ -18,14 +25,16 @@ function paintTodo(newTodo) {
   // append should be placed at the end
   li.appendChild(span); // = "li" has "span" as child code
   toDoList.appendChild(li); // = with only this, can't delete and save
-  li.appendChild(button); // make button to delete
+  li.appendChild(button); // = make button to delete
 }
 
 function onTodoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value; // copying current value into new value
   toDoInput.value = "";
+  toDos.push(newTodo);
   paintTodo(newTodo);
+  saveTodo();
 }
 
 toDoForm.addEventListener("submit", onTodoSubmit);
